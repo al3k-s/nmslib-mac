c1cc5c76d46853991916ea1e61c49b319c8fea8b
al3k-s <102021461+al3k-s@users.noreply.github.com>
2022-07-08 14:13:31 +0300

    Update distcomp_scalar.cc
    commenting out pragma for install on macos

--- similarity_search/src/distcomp_scalar.cc
+++ similarity_search/src/distcomp_scalar.cc
@@ -82,8 +82,8 @@ template float  QueryNormScalarProduct<float>(const float* pVect1, const float* 
 template <>
 float NormScalarProductSIMD(const float* pVect1, const float* pVect2, size_t qty) {
 #ifndef PORTABLE_SSE2
-#pragma message WARN("ScalarProductSIMD<float>: SSE2 is not available, defaulting to pure C++ implementation!")
-    return NormScalarProduct(pVect1, pVect2, qty);
+//#pragma message WARN("ScalarProductSIMD<float>: SSE2 is not available, defaulting to pure C++ implementation!")
+  //  return NormScalarProduct(pVect1, pVect2, qty);
 #else
     size_t qty16  = qty/16;
     size_t qty4  = qty/4;
@@ -192,8 +192,8 @@ template float  ScalarProduct<float>(const float* pVect1, const float* pVect2, s
 template <>
 float ScalarProductSIMD(const float* pVect1, const float* pVect2, size_t qty) {
 #ifndef PORTABLE_SSE2
-#pragma message WARN("ScalarProductSIMD<float>: SSE2 is not available, defaulting to pure C++ implementation!")
-    return ScalarProduct(pVect1, pVect2, qty);
+//#pragma message WARN("ScalarProductSIMD<float>: SSE2 is not available, defaulting to pure C++ implementation!")
+  //  return ScalarProduct(pVect1, pVect2, qty);
 #else
     size_t qty16  = qty/16;
     size_t qty4  = qty/4;
